a4d23f799ef26f461bea2edeb424f39ff15af68c
Katrina Verey <kn.verey@gmail.com>
2013-11-11 16:43:07 -0500

    Wrote review routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rainforest::Application.routes.draw do
+  get "reviews/show"
+  get "reviews/new"
+  get "reviews/edit"
   get "sessions/new"
   get "sessions/create"
   get "sessions/destroy"
   get "users/new"
   get "users/create"
   root 'products#index'
-  resources :products
+  resources :products do
+    resources :reviews, :except => [:index]
+  end
   resources :users, :only => [:new, :create]
   resources :sessions, :only => [:new, :create, :destroy]
 
