0486b1a03163b9b6130164c58a932d745d956c07
Katrina Verey <kn.verey@gmail.com>
2013-11-11 17:27:55 -0500

    Added username, first name and last name to User. Automatic sign-in on sign up.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111191014) do
+ActiveRecord::Schema.define(version: 20131111215551) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 20131111191014) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
   end
 
 end
