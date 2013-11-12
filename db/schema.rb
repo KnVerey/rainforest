20eb0a7426594965109628dd27e47d13a4c2f883
Katrina Verey <kn.verey@gmail.com>
2013-11-11 23:38:39 -0500

    Add photo to products and HTML/CSS for product index and new

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111215551) do
+ActiveRecord::Schema.define(version: 20131111234947) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20131111215551) do
     t.integer  "price_in_cents"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo"
   end
 
   create_table "reviews", force: true do |t|
