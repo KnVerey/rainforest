cc5689ed0e3c70cf7677c5f659abec4fee91cd9c
Katrina Verey <kn.verey@gmail.com>
2013-11-12 17:28:07 -0500

    Show links to reviewed products on user profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
 	has_many :products, :through => :reviews
 
 	validates_presence_of :first_name, :last_name, :username, :email
+	validates :email, uniqueness: true
 end
