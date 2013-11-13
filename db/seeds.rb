9660ddd82a02a0847ba80da8957aa8d4ee488f6b
Katrina Verey <kn.verey@gmail.com>
2013-11-13 13:24:43 -0500

    Tested and tweaked seeds, tweaked mailto email. About to add categories.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create(:email => 'demo1@rainforest.herokuapp.comm', :password => '12password09', :password_confirmation => '12password09', :first_name => 'Sarah', :last_name => 'Smith')
+User.create(:email => 'demo1@rainforest.herokuapp.com', :password => '12password09', :password_confirmation => '12password09', :first_name => 'Sarah', :last_name => 'Smith')
 User.create(:email => 'demo2@rainforest.herokuapp.com', :password => '12password09', :password_confirmation => '12password09', :first_name => 'John', :last_name => 'Miller')
 
 Product.create(:name => 'Kitten', :description => "She's adorable! I wish I didn't have to give her away!", :price_in_cents => 0, :photo => 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbqU_M962J8hGN0dji1Z-7kPxY_wH2p9ZxZdtTRl0I3H9g-bpR8w', :user_id => 1)
